af8057a33d627d7b42a9bcbb5d86d4c2a8dfb4b7
luvidmi <welles7777@gmail.com>
2025-05-18 23:53:15 +0700

    added clear function after activities

--- Python/RNG.py
+++ Python/RNG.py
@@ -1,4 +1,6 @@
 import random
+import os
+clear = lambda: os.system('cls')
 
 def readFiles():
     names = [str(i).split() for i in open('names.txt','r',encoding='utf-8')]
@@ -13,6 +15,7 @@ def main():
             case('0'):
                 break
             case('1'):
+                clear()
                 amount = int(input('Введите желаемое количество никнеймов: '))
                 print('\n')
                 while amount!=0:
@@ -27,27 +30,35 @@ def main():
                     print(nickName)
                     amount -= 1
             case('2'):
+                clear()
                 action = input('1 - Добавить имя в список\n2 - Добавить слово в список\nВвод: ')
                 match action:
                     case('1'):
+                        clear()
                         massNames = readFiles()[0][0]
                         massWords = readFiles()[1][0]
                         nameEdit = input('Введите имя, используя нижний регистр: ')
                         file = open('names.txt', 'a', encoding='utf-8')
                         if nameEdit in massNames:
-                            print('\nТакое имя уже есть в списке\n')
+                            clear()
+                            print(f'\nИмя {nameEdit} уже есть в списке\n')
                         else:
 
                             file.write(' ' + nameEdit)
+                            clear()
                             print(f'\nИмя {nameEdit} успешно добавлено\n')
                             file.close()
+                            
                     case('2'):
+                        clear()
                         wordEdit = input('Введите слово, используя нижний регистр: ')
                         file = open('words.txt', 'a', encoding='utf-8')
                         if wordEdit in massWords:
-                            print('\nТакое слово уже есть в списке\n')
+                            clear()
+                            print(f'\nСлово {wordEdit} уже есть в списке\n')
                         else:
                             file.write(' ' + wordEdit)
+                            clear()
                             print(f'\nСлово {wordEdit} успешно добавлено\n')
                             file.close()
 
